f9f0d20c06f51045db5ed7e46779ac0b40f53744
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-03-30 16:43:45 +0200

    Remove unused import

--- src/app/apartments/page.tsx
+++ src/app/apartments/page.tsx
@@ -4,8 +4,7 @@ import { ApartmentFilters } from "@/components/apartments-filters";
 import { ApartmentListHeader } from "@/components/apartments-list-header";
 import ShowFiltersButton from "@/components/show-filters-button";
 import SortByOptions from "@/components/sort-by-options";
-import { Button } from "@/components/ui/button";
-import { useMobile } from "@/lib/hooks";
+
 import { useState } from "react";
 
 const exampleApartments = [
