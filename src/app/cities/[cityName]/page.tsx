00aca6d16d62db6a02c72ba17b5935a7b9cd66e7
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-04-03 20:47:19 +0200

    Adjust styles

--- src/app/cities/[cityName]/page.tsx
+++ src/app/cities/[cityName]/page.tsx
@@ -40,7 +40,7 @@ export default async function CityPage({
         cityDescription={city.description}
       />
 
-      <main className="container px-4 py-8">
+      <main className="container space-y-8 px-4 py-8">
         <CityHeader cityName={city.name} />
 
         <div className="grid gap-8 lg:grid-cols-3">
