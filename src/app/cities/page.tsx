9e3e59b4b0d266aba3a9f54e024daad52840bad3
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-04-14 21:54:13 +0200

    Adjust empty state

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -3,17 +3,18 @@ import { PageHeader } from "@/components/page-headers";
 import prisma from "@/lib/db";
 
 export default async function CitiesPage() {
-  const cities = await prisma.city.findMany({
-    select: {
-      id: true,
-      name: true,
-      shortDescription: true,
-      longDescription: true,
-      coverImage: true,
-      population: true,
-      area: true,
-    },
-  });
+  // const cities = await prisma.city.findMany({
+  //   select: {
+  //     id: true,
+  //     name: true,
+  //     shortDescription: true,
+  //     coverImage: true,
+  //     population: true,
+  //     area: true,
+  //   },
+  // });
+
+  const cities = [];
 
   return (
     <div className="bg-background pb-8">
@@ -21,12 +22,8 @@ export default async function CitiesPage() {
         title="Explore Cities"
         description="Discover the perfect city for your next home"
       />
-      <main className="container px-4 py-8">
-        {cities.length === 0 ? (
-          <CitiesEmptyState />
-        ) : (
-          <CityList cities={cities} />
-        )}
+      <main className="container flex flex-1 flex-col px-4 py-8">
+        <CityList cities={cities} />
       </main>
     </div>
   );
--- src/components/city-list.tsx
+++ src/components/city-list.tsx
@@ -1,10 +1,23 @@
+import { Decimal } from "@prisma/client/runtime/library";
 import { CityCard } from "./city-card";
+
+type City = {
+  id: string;
+  name: string;
+  shortDescription: string;
+  coverImage: string;
+  area: Decimal;
+};
 
 type CityListProps = {
   cities: City[];
 };
 
 export default function CityList({ cities }: CityListProps) {
+  if (cities.length === 0) {
+    return <CitiesEmptyState />;
+  }
+
   return (
     <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {cities.map((city) => (
@@ -14,13 +27,10 @@ export default function CityList({ cities }: CityListProps) {
   );
 }
 
-export function CitiesEmptyState() {
+function CitiesEmptyState() {
   return (
-    <div className="flex flex-col items-center justify-center rounded-lg border border-dashed p-8 text-center">
-      <h3 className="mb-2 text-lg font-medium">No cities found</h3>
-      <p className="text-muted-foreground">
-        Try adjusting your search criteria
-      </p>
+    <div className="rounded-lg border border-dashed bg-red-300/10 p-8 text-center">
+      <h3 className="text-lg font-medium">No cities found</h3>
     </div>
   );
 }
