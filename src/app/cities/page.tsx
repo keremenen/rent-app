fb0667673adb94078615096818e8e3ea29649c75
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-04-14 21:54:36 +0200

    Adjust empty state

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -1,20 +1,18 @@
-import CityList, { CitiesEmptyState } from "@/components/city-list";
+import CityList from "@/components/city-list";
 import { PageHeader } from "@/components/page-headers";
 import prisma from "@/lib/db";
 
 export default async function CitiesPage() {
-  // const cities = await prisma.city.findMany({
-  //   select: {
-  //     id: true,
-  //     name: true,
-  //     shortDescription: true,
-  //     coverImage: true,
-  //     population: true,
-  //     area: true,
-  //   },
-  // });
-
-  const cities = [];
+  const cities = await prisma.city.findMany({
+    select: {
+      id: true,
+      name: true,
+      shortDescription: true,
+      coverImage: true,
+      population: true,
+      area: true,
+    },
+  });
 
   return (
     <div className="bg-background pb-8">
