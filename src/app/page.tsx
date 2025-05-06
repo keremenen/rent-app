36a8eaa029dca4c6d0ade22373e2829f82ca9c5a
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-06 14:51:41 +0200

    Remove newsletter files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { FeaturedApartments } from "@/components/featured-apartments";
 import { HeroSection } from "@/components/hero-section";
 import { HowItWorks } from "@/components/how-it-works";
-import { Newsletter } from "@/components/newsletter";
 import { PopularNeighborhoods } from "@/components/popular-neighborhoods";
 import { PropertyOwners } from "@/components/property-owners";
 import { Testimonials } from "@/components/testimonials";
@@ -29,7 +28,6 @@ export default async function Home() {
       <PopularNeighborhoods neighborhoods={popularNeighborhoods} />
       <PropertyOwners />
       <Testimonials />
-      {/* <Newsletter /> */}
     </main>
   );
 }
