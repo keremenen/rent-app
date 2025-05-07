262a029a51b966a63dc3e7fd3375a999a553f00f
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-07 16:51:58 +0200

    Add search params

--- src/app/neighborhoods/[neighborhoodId]/page.tsx
+++ src/app/neighborhoods/[neighborhoodId]/page.tsx
@@ -5,40 +5,38 @@ import NeighborhoodFeatures from "@/components/neighborhood-features";
 import { NeighborhoodHero } from "@/components/neighborhood-hero";
 import { NeighborhoodStats } from "@/components/neighborhood-stats";
 import { useApartmentContext, useNeighborhoodContext } from "@/lib/hooks";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 
-type NeighborhoodParams = {
-  params: Promise<{ neighborhoodId: string }>;
-};
+type Params = Promise<{ neighborhoodId: string }>;
 
-export default function NeighborhoodPage({ params }: NeighborhoodParams) {
-  const tmpNeighborHoodId = "srodmiescie-gdansk";
+export default function NeighborhoodPage(props: { params: Params }) {
+  const { neighborhoodId } = use(props.params);
+
   const { handleSetSelectedNeighborhood, selectedNeighborhood } =
     useNeighborhoodContext();
 
   const { getAllApartmentsInNeighborhood } = useApartmentContext();
-  const apartments = getAllApartmentsInNeighborhood(tmpNeighborHoodId);
-  console.log("apartments", apartments);
+  const apartments = getAllApartmentsInNeighborhood(neighborhoodId);
 
   useEffect(() => {
-    handleSetSelectedNeighborhood(tmpNeighborHoodId);
-  }, [handleSetSelectedNeighborhood]);
+    handleSetSelectedNeighborhood(neighborhoodId);
+  }, [handleSetSelectedNeighborhood, neighborhoodId]);
 
   if (!selectedNeighborhood) return null;
 
   return (
     <div className="bg-background">
-      <NeighborhoodHero id={tmpNeighborHoodId} />
+      <NeighborhoodHero id={neighborhoodId} />
       <main className="container space-y-6 px-4 py-8">
         <div className="grid gap-6 md:grid-cols-2">
-          <NeighborhoodStats id={tmpNeighborHoodId} />
+          <NeighborhoodStats id={neighborhoodId} />
           <NeighborhoodFeatures />
         </div>
         <div className="grid gap-6 lg:grid-cols-[300px_1fr]">
           <div>{/* <ApartmentFilters /> */}</div>
 
           <div>
-            <NeighborhoodApartments neighborhoodId={tmpNeighborHoodId} />
+            <NeighborhoodApartments neighborhoodId={neighborhoodId} />
           </div>
         </div>
       </main>
