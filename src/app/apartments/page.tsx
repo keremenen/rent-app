49c9d0ec44cbbfe25954dbd72eab71ab8093954d
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-07 10:52:41 +0200

    WIP add apartment context to apartments page

--- src/app/apartments/page.tsx
+++ src/app/apartments/page.tsx
@@ -1,54 +1,27 @@
+"use client";
 import { ApartmentCard } from "@/components/apartment-card";
 import { ApartmentFilters } from "@/components/apartments-filters";
 import { ApartmentListHeader } from "@/components/apartments-list-header";
-import {
-  convertApartmentsToPlain,
-  generateFilterObject,
-  generatePrismaFilters,
-  getApartmentsByFilters,
-} from "@/lib/utils";
+import { useApartmentContext } from "@/lib/hooks";
+import {} from // convertApartmentsToPlain,
+// generateFilterObject,
+// generatePrismaFilters,
+// getApartmentsByFilters,
+"@/lib/utils";
 
-type SearchParams = { [key: string]: string | undefined };
-
-export default async function ApartmentsListPage(props: {
-  searchParams: SearchParams;
-}) {
-  const { minprice, maxprice, bedrooms, amenities, availability } =
-    await props.searchParams;
-
-  // Generate Prisma filters based on the search parameters
-  const prismaFilters = generatePrismaFilters({
-    minprice,
-    maxprice,
-    bedrooms,
-    amenities,
-  });
-
-  // Generate filter object for the UI based on the search parameters
-  const filterObject = generateFilterObject({
-    minprice,
-    maxprice,
-    bedrooms,
-    amenities,
-    availability,
-  });
-
-  // Fetch apartments based on the generated Prisma filters
-  const apartments = await getApartmentsByFilters(prismaFilters);
-
-  // Convert apartments to a plain format for easier rendering
-  const plainApartments = convertApartmentsToPlain(apartments);
+export default function ApartmentsListPage() {
+  const { apartments } = useApartmentContext();
 
   return (
     <div className="bg-background">
-      <ApartmentListHeader totalCount={plainApartments.length} />
+      <ApartmentListHeader totalCount={apartments.length} />
       <main className="container px-4 py-8">
         <div className="grid gap-6 lg:grid-cols-[300px_1fr]">
           <div className={`space-y-6 lg:block`}>
             <ApartmentFilters
               priceRange={[1000, 4000]}
               priceRangeInitialValues={[1800, 2000]}
-              filters={filterObject}
+              // filters={filterObject}
               checkboxSections={[
                 {
                   sectionName: "bedrooms",
@@ -67,8 +40,8 @@ export default async function ApartmentsListPage(props: {
 
           <div>
             <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-              {plainApartments.length > 0 ? (
-                plainApartments.map((apartment) => (
+              {apartments.length > 0 ? (
+                apartments.map((apartment) => (
                   <ApartmentCard key={apartment.id} apartment={apartment} />
                 ))
               ) : (
