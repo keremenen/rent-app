b6b26b1eb6dfbe502fbd2337f26d1b9e80f10fe1
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-09 12:39:32 +0200

    WIP edit images section

--- src/components/admin/apartment-form.tsx
+++ src/components/admin/apartment-form.tsx
@@ -20,11 +20,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { apartmentFormSchema, TApartmentForm } from "@/lib/validations";
 import { Button } from "../ui/button";
 import { AMENITIES_DATA, NEIGHBORHOODS_DATA } from "@/lib/constants";
-import { Save } from "lucide-react";
+import { ImagePlus, Save, Trash2, Upload } from "lucide-react";
 import { useApartmentContext } from "@/lib/hooks";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "../ui/utils";
 import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
+import Image from "next/image";
 
 type ApartmentFormProps = {
   actionType: "add" | "edit";
@@ -93,7 +95,9 @@ export default function ApartmentForm({
             errors={errors}
           />
         </TabsContent>
-        <TabsContent value="images" className="space-y-4 pt-4"></TabsContent>
+        <TabsContent value="images" className="space-y-4 pt-4">
+          <ImagesSection apartment={apartment} />
+        </TabsContent>
       </Tabs>
 
       <div className="flex gap-2">
@@ -323,6 +327,62 @@ function AmenitiesSection({
           </div>
         </GridItem>
       ))}
+    </section>
+  );
+}
+
+function ImagesSection({ apartment }: { apartment: TApartmentForm }) {
+  return (
+    <section className="space-y-4">
+      <div className="flex flex-col">
+        <h3 className="text-lg font-medium">Thumbnail Image</h3>
+        <p className="text-muted-foreground text-sm">
+          This image will be used as the main image for the apartment listing.
+        </p>
+
+        <div className="mt-4">
+          <div className="relative h-[250px] w-1/3">
+            <Image src={apartment.thumbnail} fill alt="cos tam" />
+          </div>
+          {/* <div className="relative">
+            <div className="absolute right-2 bottom-2 flex gap-2">
+              <Button variant="secondary" size="sm">
+                <Upload className="mr-2 h-4 w-4" />
+                Change
+              </Button>
+            </div>
+          </div> */}
+        </div>
+      </div>
+
+      <Separator />
+      <div>
+        <h3 className="text-lg font-medium">Gallery Images</h3>
+        <p className="text-muted-foreground text-sm">
+          Add multiple images to showcase the apartment.
+        </p>
+
+        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          {Array(8).map((image, index) => (
+            <div key={index} className="relative">
+              <Button
+                variant="destructive"
+                size="icon"
+                className="absolute top-2 right-2"
+              >
+                <Trash2 className="h-4 w-4" />
+              </Button>
+            </div>
+          ))}
+
+          <div className="flex h-[150px] w-full items-center justify-center rounded-md border border-dashed">
+            <Button variant="outline">
+              <ImagePlus className="mr-2 h-4 w-4" />
+              Add Image
+            </Button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
