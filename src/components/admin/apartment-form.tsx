1c7b2e649c87202210da45f67b22123dadeb89e0
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-12 16:41:18 +0200

    Remove unused code

--- src/components/admin/apartment-form.tsx
+++ src/components/admin/apartment-form.tsx
@@ -1,13 +1,6 @@
 "use client";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
 import { Textarea } from "../ui/textarea";
 import {
   Control,
@@ -19,11 +12,10 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { apartmentFormSchema, TApartmentForm } from "@/lib/validations";
 import { Button } from "../ui/button";
-import { AMENITIES_DATA, NEIGHBORHOODS_DATA } from "@/lib/constants";
-import { ImagePlus, Save, Trash2 } from "lucide-react";
+import { AMENITIES_DATA } from "@/lib/constants";
+import { Save } from "lucide-react";
 import { cn } from "../ui/utils";
 import { Checkbox } from "../ui/checkbox";
-import { Separator } from "../ui/separator";
 import {
   Card,
   CardContent,
@@ -117,8 +109,6 @@ export default function ApartmentForm(props: ApartmentFormProps) {
         errors={errors}
       />
 
-      {/* <ImagesSection apartment={apartment} /> */}
-
       <div className="flex gap-2">
         <Button variant="outline">Cancel</Button>
         <Button type="submit">
@@ -132,8 +122,6 @@ export default function ApartmentForm(props: ApartmentFormProps) {
 
 function DetailsSection({
   register,
-  apartment,
-  control,
   errors,
 }: {
   register: UseFormRegister<TApartmentForm>;
@@ -275,11 +263,7 @@ type AmenitiesSectionProps = {
   errors: FieldErrors<TApartmentForm>;
 };
 
-function AmenitiesSection({
-  apartment,
-  control,
-  errors,
-}: AmenitiesSectionProps) {
+function AmenitiesSection({ control, errors }: AmenitiesSectionProps) {
   type handleAmenityChangeType = {
     checked: boolean;
     amenityId: string;
@@ -341,62 +325,6 @@ function AmenitiesSection({
           ))}
         </CardContent>
       </Card>
-    </section>
-  );
-}
-
-function ImagesSection({ apartment }: { apartment: TApartmentForm }) {
-  return (
-    <section className="space-y-4">
-      <div className="flex flex-col">
-        <h3 className="text-lg font-medium">Thumbnail Image</h3>
-        <p className="text-muted-foreground text-sm">
-          This image will be used as the main image for the apartment listing.
-        </p>
-
-        <div className="mt-4">
-          {/* <div className="relative h-[250px] w-1/3">
-            <Image src={apartment.thumbnail} fill alt="cos tam" />
-          </div> */}
-          {/* <div className="relative">
-            <div className="absolute right-2 bottom-2 flex gap-2">
-              <Button variant="secondary" size="sm">
-                <Upload className="mr-2 h-4 w-4" />
-                Change
-              </Button>
-            </div>
-          </div> */}
-        </div>
-      </div>
-
-      <Separator />
-      <div>
-        <h3 className="text-lg font-medium">Gallery Images</h3>
-        <p className="text-muted-foreground text-sm">
-          Add multiple images to showcase the apartment.
-        </p>
-
-        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          {Array(8).map((image, index) => (
-            <div key={index} className="relative">
-              <Button
-                variant="destructive"
-                size="icon"
-                className="absolute top-2 right-2"
-              >
-                <Trash2 className="h-4 w-4" />
-              </Button>
-            </div>
-          ))}
-
-          <div className="flex h-[150px] w-full items-center justify-center rounded-md border border-dashed">
-            <Button variant="outline">
-              <ImagePlus className="mr-2 h-4 w-4" />
-              Add Image
-            </Button>
-          </div>
-        </div>
-      </div>
     </section>
   );
 }
